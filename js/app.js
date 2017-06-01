d3e6e99b6049bf07afea590d798798d2d8e21c88
MontserratLili <lauraliligm@gmail.com>
2017-06-01 17:14:32 -0500

    funcionalidad jQuery

--- js/app.js
+++ js/app.js
@@ -1,45 +1,33 @@
-var target = 0;
+$(document).ready(function () {
+    var target = 0;
 
-var cargarPagina = function () {
-	// Elementos
-	var botones = document.querySelectorAll(".control");
-	var anterior = document.querySelector(".previous");
-	var siguiente = document.querySelector(".next");
-	
-	// Eventos
-	botones.forEach(function (boton) {
-		boton.addEventListener("click", cambiarImagen);
-	});
-	anterior.addEventListener("click", anteriorImagen);
-	siguiente.addEventListener("click", siguienteImagen);
-};
+    var $botones = $(".control");
+    var $anterior = $(".previous");
+    var $siguiente = $(".next");
+    var $target = parseInt($(this.dataset.target));
 
-var cambiarImagen = function () {
-	target = parseInt(this.dataset.target);
-	mostrarImagen(target);
-};
+    $(".botones").each(function () {
+        $(".boton").click(function () {
+            $(".boton").click(function (target) {
+                var $lastSlide = $("div.active");
+                var $slide = $("div[data-slide='" + target + "']");
+                $lastSlide.removeClass(".active");
+                $slide.addClass(".active");
+            });
+        });
+    });
 
-var mostrarImagen = function (target) {
-	var lastSlide = document.querySelector("div.active");
-	var slide = document.querySelector("div[data-slide='" + target + "']");
-	lastSlide.classList.remove("active");
-	slide.classList.add("active");
-};
-
-var anteriorImagen = function (e) {
-	e.preventDefault();
-	target = target - 1;
-	target = (target < 0) ? 3 : target;
-	mostrarImagen(target);
-};
-
-var siguienteImagen = function (e) {
-	e.preventDefault();
-	target = target + 1;
-	target = (target > 3) ? 0 : target;
-	mostrarImagen(target);mostrarImagen(target);
-};
-
-
-
-window.addEventListener("load", cargarPagina);
+    $(".previous").click(function (e) {
+        e.preventDefault();
+        target = target - 1;
+        target = (target < 0) ? 3 : target;
+        mostrarImagen(target);
+    });
+    
+     $(".next").click(function (e) {
+        e.preventDefault();
+        target = target + 1;
+        target = (target >3) ? 0 : target;
+        mostrarImagen(target);
+    });
+});
